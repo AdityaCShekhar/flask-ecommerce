62dc114dfec4c7cf0973c31cd556b39d988d867d
Shashank P R <138097341+pr2003-Shashank@users.noreply.github.com>
2025-04-14 14:36:42 +0530

    Delete models directory

--- models/db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["ecommerce"]
-
-users_col = db["users"]
-products_col = db["products"]
-categories_col = db["categories"]
-cart_col = db["cart"]
